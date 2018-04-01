cbb289e74860e571313f34bbab84452c191a831d
Vacster <kamil@unitec.edu>
2018-04-01 13:40:19 -0600

    Added spaces after string

--- Arrays.jl
+++ Arrays.jl
@@ -2,10 +2,10 @@ x::Array{Int} = [15, 34, 25, 54]
 
 for a = 1:4
     if x[a] > 25
-        println("Mayor a 25", x[a])
+        println("Mayor a 25 ", x[a])
     elseif x[a] < 25
-        println("Menor a 25", x[a])
+        println("Menor a 25 ", x[a])
     else
-        println("Igual a 25", x[a])
+        println("Igual a 25 ", x[a])
     end
 end
