b85dccb05a17802eac58c4a753822259b76c7592
Raim <rai.interiano@gmail.com>
2018-04-01 17:46:20 -0600

    single comments file

--- comments.jl
+++ comments.jl
@@ -6,3 +6,16 @@
 println(1)
 
 # this file has no code at all
+
+#=    Whell
+  Dhez es a black fool of kamments
+  Yeno de herrorez
+  Pra quen quizer ler
+  Tem julia code como this: x:Int32 = 0;
+=#
+
+println(11)
+
+#= Just do it. =#
+
+#= This file is codeless too. =#
