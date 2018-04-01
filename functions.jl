77b142ff553b1bd31c2035a6a29e78fa5de4ae24
Vacster <kamil@unitec.edu>
2018-04-01 14:20:06 -0600

    Removing bubble sort since it's in another example

--- functions.jl
+++ functions.jl
@@ -8,20 +8,6 @@ function func(x::Int, y::Int, z::Int)::Int
 			println("Hola")
 		end
 	end
-
-	a::Array{Int} = [7,4,9,2,10,1,5,3,8,6,false]
-
-	for i = 1:11
-	    for j = 1:11-1
-	        if a[j] > a[j+1]
-	            temp::Int = a[j+1]
-	            a[j+1] = a[j]
-	            a[j]=temp
-	        end
-	    end
-	end
-
-
 	return result
 end
 
