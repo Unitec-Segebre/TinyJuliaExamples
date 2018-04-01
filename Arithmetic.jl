015f762fe06647294f93555cba3477ae4fdf8684
Raim <rai.interiano@gmail.com>
2018-04-01 17:45:34 -0600

    removing spaces in Arithmetic

--- Arithmetic.jl
+++ Arithmetic.jl
@@ -7,15 +7,15 @@
 x::Int = 10
 y::Int = 3
 
-println (x)
-println (y)
-println (-x)
-println (-y);println (x + y)
-println (x - y)
-println (x * y)
-println (x / y)
-println (x % y)
-println (x ^ y)
+println(x)
+println(y)
+println(-x)
+println(-y);println (x + y)
+println(x - y)
+println(x * y)
+println(x / y)
+println(x % y)
+println(x ^ y)
 
 
 #=
