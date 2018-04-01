f436ca47b97d8ce11c77330f899a0ddc3631c8e7
Raim <rai.interiano@gmail.com>
2018-04-01 17:51:26 -0600

    functions in single file

--- functions.jl
+++ functions.jl
@@ -11,4 +11,10 @@ function func(x::Int, y::Int, z::Int)::Int
 	return result
 end
 
+function addition(w::Int, x::Int, y::Int, z::Int)::Int
+    return w+x+y+z;
+end
+
+println(addition(5, 5^2, 5<<2, 50/5))
+
 println(func(1, 2, 3))
